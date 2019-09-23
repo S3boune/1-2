ee07cc5f67922d84feb45e0b8e30e32d0125709f
Sebastien <seb.morizot@gmail.com>
2019-09-23 06:57:42 +0200

    Pyramide with while

--- exo_ruby/exo_01.rb
+++ exo_ruby/exo_01.rb
@@ -2,12 +2,12 @@ puts "Donne moi un nombre entre 1 et 25"
 answer = gets.chomp
 
 num = answer.to_i
+piece = "#"
+space = " "
+i = 1
 
-num.times do |i|
-  piece = "#"
-  space = " "
-
-  floor = (space * (num-i)) + piece * (i+1)
-
+while i <= num
+  floor = (space * (num - i)) + (piece * i)
   puts floor
+  i+= 1
 end
